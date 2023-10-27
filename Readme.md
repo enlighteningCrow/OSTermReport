79a0080b2a04b3832a6d45c0eea87ad75ac2cda0
enlighteningCrow <87510657+enlighteningCrow@users.noreply.github.com>
2023-10-27 22:36:58 +0700

    Removed username from scripts

--- Readme.md
+++ Readme.md
@@ -2,5 +2,5 @@ Usage:
 To run the hangman servers or clients, follow the instructions in the comments of runImages.sh.
 
 To use both the server and client as docker containers, first run:
-docker network create hangman
+`docker network create hangman`
 Then follow the instructions in the comments of runImages.sh
--- runImages.sh
+++ runImages.sh
@@ -1,20 +1,26 @@
+#Do not run this file. This is a collection of commands to run the clients and servers in docker and natively.
+#Use this by copying one command into the terminal manually.
+
+#Use this command before running the first four commands below to create the hangman network.
+docker network create hangman
+
 #Use this command to run the python server docker image to connect to clients running in other docker containers.
-docker run --rm --net hangman --name hangmanServer enlighteningcrow/hangman_py_server:latest
+docker run --rm --net hangman --name hangmanServer hangman_py_server:latest
 
 #Use this command to run the python client docker image to connect to a server running in another docker container.
-docker run --rm --net hangman --name hangmanClient --interactive enlighteningcrow/hangman_py_client:latest
+docker run --rm --net hangman --name hangmanClient --interactive hangman_py_client:latest
 
 #Use this command to run the java server docker image to connect to clients running in other docker containers.
-docker run --rm --net hangman --name hangmanServer enlighteningcrow/hangman_java_server:latest
+docker run --rm --net hangman --name hangmanServer hangman_java_server:latest
 
 #Use this command to run the java client docker image to connect to a server running in another docker container.
-docker run --rm --net hangman --name hangmanClient --interactive enlighteningcrow/hangman_java_client:latest
+docker run --rm --net hangman --name hangmanClient --interactive hangman_java_client:latest
 
 #Use this command to run the python server docker image to connect to clients running natively on the machine.
-docker run --rm --name hangmanServer -p 1943:1943 enlighteningcrow/hangman_py_server:latest
+docker run --rm --name hangmanServer -p 1943:1943 hangman_py_server:latest
 
 #Use this command to run the java server docker image to connect to clients running natively on the machine.
-docker run --rm --name hangmanServer -p 1943:1943 enlighteningcrow/hangman_java_server:latest
+docker run --rm --name hangmanServer -p 1943:1943 hangman_java_server:latest
 
 #Use this command to run the python server natively
 cd $(git rev-parse --show-toplevel)/hangmanPy/;python server.py
