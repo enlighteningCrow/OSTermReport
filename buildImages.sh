afc9628d1c817f6fae542851064714be0723845d
enlighteningCrow <87510657+enlighteningCrow@users.noreply.github.com>
2023-10-27 22:45:13 +0700

    Update buildImages.sh

--- buildImages.sh
+++ buildImages.sh
@@ -1,5 +1,5 @@
 # This is the script to build all the docker images
-
+cd $(git rev-parse --show-toplevel)
 docker build --tag hangman_py_server --no-cache -f hangmanPy/server.Dockerfile ./hangmanPy/
 docker build --tag hangman_py_client --no-cache -f hangmanPy/client.Dockerfile ./hangmanPy/
 gradle build -p ./hangmanJava/
